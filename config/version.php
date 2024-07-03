0ce69e9d845ec3f6d11a6b56708eac8a3c5bc1a8
Minh Luan <minhluan1875@gmail.com>
2024-07-03 15:47:17 +0700

    dowload1

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v7.0.6',
+  'full_app_version' => 'v7.0.6 - build 14112-ge1a6b441d',
+  'build_version' => '14112',
+  'prerelease_version' => '',
+  'hash_version' => 'ge1a6b441d',
+  'full_hash' => 'v7.0.6-6-ge1a6b441d',
+  'branch' => 'master',
+);
